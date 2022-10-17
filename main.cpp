47b5f54e59a0a4f4d1e8245f2ceed6f252db15c4
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2022-10-17 18:24:59 +0000

    added code

--- main.cpp
+++ main.cpp
@@ -5,11 +5,30 @@
 
 #include "mbed.h"
 
+#define FULL_CIRCLE 360
+#define TIME_FOR_360 1450
+
+DigitalOut bl(PC_0);
+DigitalOut br(PC_6);
+DigitalOut fl(PC_1);
+DigitalOut fr(PC_7);
+
+void rotate(float deg) {
+	fl = true;
+	br = true;
+
+	float tmp = (deg / FULL_CIRCLE) * TIME_FOR_360;
+	ThisThread::sleep_for((int) tmp);
+
+	fl = false;
+	br = false;
+}
 
 DigitalOut led(LED1);
 
 int main() {
-
+	rotate(360);
+	// fr = true;
     while (true) {
         ThisThread::sleep_for(200ms);
         led = !led;
